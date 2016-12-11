2d6b46562dc7341f5ed2dbf9af80e4536f8b6c2b
Jesslyn Landgren <jesslyn.landgren@gmail.com>
2016-12-11 15:57:09 -0500

    Added same firefox filter size fix to mobile

--- static/js/main.js
+++ static/js/main.js
@@ -33,12 +33,27 @@ $(window).resize(function(){
     var windowHeight = $(window).height();
     $('.tag').css('height', (windowHeight - navHeight));
     $('.tag').css('padding-top', navHeight);
-    var skillsHeight = $('.skills').height();
-    $('.filter-skills').css('height', skillsHeight);
-    var contactHeight = $('.contact').height();
-    $('.filter-contact').css('height', contactHeight);
+
     var tagHeight = $('.tag').height();
     $('.filter-tag').css('height', tagHeight);
-    var iconWidth = $('.skill-icon').width();
-    $('.icon-grid').css('max-width', iconWidth);
+    var tagTop = $('.tag').offset().top;
+    var tagLeft = $('.tag').offset().left;
+    $('.filter-tag').css('top', tagTop);
+    $('.filter-tag').css('left', tagLeft);
+    $('.filter-tag').css('padding-top', navHeight);
+
+    var skillsHeight = $('.skills').height();
+    $('.filter-skills').css('height', skillsHeight);
+    var skillsTop = $('.skills').offset().top;
+    var skillsLeft = $('.skills').offset().left;
+    $('.filter-skills').css('top', skillsTop);
+    $('.filter-skills').css('left', skillsLeft);
+
+
+    var contactHeight = $('.contact').height();
+    $('.filter-contact').css('height', contactHeight);
+    var contactTop = $('.contact').offset().top;
+    var contactLeft = $('.contact').offset().left;
+    $('.filter-contact').css('top', contactTop);
+    $('.filter-contact').css('left', contactLeft);
 });
